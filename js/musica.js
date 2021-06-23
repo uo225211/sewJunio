9c6652379c89ebb095cc1b293faf3eadf9245b2b
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-06-23 21:16:13 +0200

    c

--- js/musica.js
+++ js/musica.js
@@ -52,6 +52,50 @@ comprobarPlay(){
 }
 
 var musica= new Musica();
+class Navegador{
+    constructor(){ 
+	
+      this.audio=document.getElementById("audio");   
+      this.audio.autoplay = "true";
+      this.audioloop=true;
+      this.audio.volume = 0.5; 
+    }
+    open() {
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("myOverlay").style.display = "block";
+      }
+       
+    close() {
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("myOverlay").style.display = "none";
+      }
+    
+      play() {
+        if (document.getElementById("pause/play").getAttribute("src")=="multimedia/play.png"){
+          this.audio.play();
+          document.getElementById("pause/play").setAttribute("src", "multimedia/pause.png");
+        }
+        else{
+          this.audio.play();
+          document.getElementById("pause/play").setAttribute("src", "multimedia/play.png");
+        }
+             
+      }
+        
+      sound(){
+        if (document.getElementById("sound/notSound").getAttribute("src")=="multimedia/sound.png"){
+          this.audio.volume = 0;
+          document.getElementById("sound/notSound").setAttribute("src", "multimedia/notsound.png");
+        }
+        else{
+          this.audio.volume = 0.7;
+          document.getElementById("sound/notSound").setAttribute("src", "multimedia/sound.png");
+        }
+             
+      }
+}
 
+
+var nave = new  Navegador();
 
 
--- js/sitios.js
+++ js/sitios.js
@@ -1,41 +1,50 @@
-var miMapa = new Object();
-
-function initMap(){  
-    var centro = {lat: 43.3672702, lng: -5.8502461};
-    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'),{
-        zoom: 8,
-        center:centro,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    var gijon =  {lat: 43.5439586, lng: -5.6732480}
-    var marcador2 = new google.maps.Marker({title: 'El destino del Bierzo',position:gijon,map:mapaGeoposicionado});
-    infoWindow = new google.maps.InfoWindow;
-    if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Tu posición actual');
-            infoWindow.open(mapaGeoposicionado);
-            mapaGeoposicionado.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+"use strict";
+class Geolocalización {
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
+    }
+    getPosicion(posicion){
+        this.mensaje = "Se ha realizado correctamente la petición de geolocalización";
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
         }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: Ha fallado la geolocalización' :
-                              'Error: Su navegador no soporta geolocalización');
-        infoWindow.open(mapaGeoposicionado);
-      }
-
-miMapa.initMap = initMap;
+    }
+    getLongitud(){
+        return this.longitud;
+    }
+    getLatitud(){
+        return this.latitud;
+    }
+    getAltitud(){
+        return this.altitud;
+    }
+    irGoogle(){
+        var url='https://www.google.es/maps/dir/ ';
+        url+=this.getLatitud +','+ this.getLongitud;
+        url+='/43.5439586,+-5.6732480/@43.4614307,-5.8320783,12z/data=!3m1!4b1!4m10!4m9!1m3!2m2!1d-5.8502461!2d43.3672702!1m3!2m2!1d-5.673248!2d43.5439586!3e0?hl=es&authuser=0'
+        var win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();  
+    }
+}
+var sitio = new Geolocalización();
